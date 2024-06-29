72dd422daa238e96185cd4386534cde0c5032f52
George William Sykes <sykes.george23@gmail.com>
2024-06-29 08:58:48 +0200

    Ahh cors

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 async function request_weather() {
     const body_text = document.getElementById("body_text");
 
-    let sitename = "https://djurze.github.io/ https://github.com/Djurze";
+    //let sitename = "https://djurze.github.io/ https://github.com/Djurze";
 
     const request = "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=58.4699&lon=7.1994";
 
@@ -9,12 +9,16 @@ async function request_weather() {
         "User-Agent": sitename,
     };
 
+    /*
     let response = await fetch(request, {
         headers: {
             "User-Agent": sitename
         }
     });
+    */
 
+    let response = await fetch(request);
+    
     if (response.ok) {
         let json = await response.json();
         body_text.innerText = json;
